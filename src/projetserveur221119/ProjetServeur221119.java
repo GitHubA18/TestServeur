5dedf2411c030d79ae584a871fc8a2de686f6ebf
GitHubA18 <56780224+GitHubA18@users.noreply.github.com>
2019-11-22 18:15:25 -0500

    Commit v2

--- src/projetserveur221119/ProjetServeur221119.java
+++ src/projetserveur221119/ProjetServeur221119.java
@@ -16,6 +16,8 @@ public class ProjetServeur221119 {
      */
     public static void main(String[] args) {
         System.out.println("Bonjour"+args[1]+","+args[0]);
+        System.out.println("Au revoir !");
+        System.out.println("Merci!");
     }
     
 }
